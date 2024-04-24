82056fee1dff15618192aaff17405d0921fce74b
Anagha R <anagharanjith2@gmail.com>
2024-04-24 21:46:01 +0530

    done

--- Estate-Server/index.js
+++ Estate-Server/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+//database
+mongoose.connect(process.env.DB_CONNECTION).then(()=>{
+    console.log("connected to database...")
+}).catch((err)=>{
+    console.log(err);
+})
+
+//server
 const app = express();
-
 app.listen(3000,()=>{
-    console.log(' Server is running on port 3000!!!no')
+    console.log(' Server is running on port 3000!!!')
 })
